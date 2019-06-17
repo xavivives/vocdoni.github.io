d863491c700198f69f5f1ad20223c1aca4ae71a6
Xavi <xavings@gmail.com>
2019-06-17 10:17:53 +0200

    Updates

---
+++ precache-manifest.829ecc8001c74c3ce5a515814bb5d80c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1af952fefa075180869f3083725c536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6178aa9f5c06426014c",
+    "url": "/static/css/main.4cfe58e7.chunk.css"
+  },
+  {
+    "revision": "c079cfb01c20b4845727",
+    "url": "/static/js/2.8cc9deb5.chunk.js"
+  },
+  {
+    "revision": "b6178aa9f5c06426014c",
+    "url": "/static/js/main.43ac6127.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/Analytics.js
+++
@@ -1,9 +0,0 @@
-export default function track(eventName){
-    console.log("tracking");
-    if(window.mixpanel)
-        return
-
-    console.log("tracking", eventName)
-    window.mixpanel.track(eventName,{level:1} );
-
-}
